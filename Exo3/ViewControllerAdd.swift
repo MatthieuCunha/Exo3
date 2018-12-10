cee8bc96ce037b5aa812db4d952ed36dda327497
VM <vm@VMs-Mac.local>
2018-12-10 18:25:30 +0100

    azertomp

---
+++ Exo3/ViewControllerAdd.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerAdd.swift
+//  Exo3
+//
+//  Created by VM on 10/12/2018.
+//  Copyright © 2018 VM. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAdd: UIViewController {
+
+    @IBOutlet weak var text1: UITextField!
+    
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBOutlet weak var text3: UITextField!
+    
+    
+    @IBOutlet weak var text4: UITextField!
+    
+    @IBAction func valider() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
